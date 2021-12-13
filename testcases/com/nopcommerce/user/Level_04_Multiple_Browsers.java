110f581d94fe7d8f94b6357f2e84308dcbb1670d
Trinh Ngoc Minh <tnminh23@gmail.com>
2021-12-13 22:10:38 +0700

    Run on multiple browsers

---
+++ testcases/com/nopcommerce/user/Level_04_Multiple_Browsers.java
@@ -0,0 +1,80 @@
+package com.nopcommerce.user;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BasePage;
+import commons.BaseTest;
+import pageObjects.HomePageObject;
+import pageObjects.RegisterPageObject;
+
+public class Level_04_Multiple_Browsers extends BaseTest{
+	private WebDriver driverTestClass;
+	private String firstName, lastName, password;
+	private HomePageObject homePage;
+	private RegisterPageObject registerPage;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driverTestClass = getBrowserDriver(browserName);
+
+		homePage = new HomePageObject(driverTestClass);
+		firstName = "Min";
+		lastName = "Trinh";
+		password = "123456";
+	}
+
+	@Test
+	public void Register_01_Empty_Data() {
+		homePage.clickToRegisterLink();
+
+		registerPage = new RegisterPageObject(driverTestClass);
+
+		registerPage.clickToRegisterButton();
+
+		Assert.assertEquals(registerPage.getErrorMessageAtFirstnameTextbox(), "First name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtLastnameTextbox(), "Last name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Email is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtPasswordTextbox(), "Password is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtConfirmPasswordTextbox(), "Password is required.");
+	}
+
+	@Test
+	public void Register_02_Invalid_Email() {
+		homePage.clickToRegisterLink();
+
+		registerPage = new RegisterPageObject(driverTestClass);
+
+		registerPage.inputToFirstnameTextbox(firstName);
+		registerPage.inputToLastnameTextbox(lastName);
+		registerPage.inputToEmailTextbox("123@123@");
+		registerPage.inputToPasswordTextbox(password);
+		registerPage.inputToConfirmPasswordTextbox(password);
+
+		registerPage.clickToRegisterButton();
+
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Wrong email");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driverTestClass.quit();
+	}
+
+	public int generateFakeNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+
+}
